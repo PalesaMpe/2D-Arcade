9492a75613fd5d5edcf49ff225bb89070b5ccd2f
PalesaMpe <mpalesa.mpe@gmail.com>
2022-02-28 00:18:03 +0200

    added movement

---
+++ 2dArcade/Assets/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float jumpSpeed = 10f;
+    // public GameObject TheBall;
+    Rigidbody2D rb;
+    void Update()
+    {
+       if (Input.GetKey(KeyCode.LeftArrow))
+         transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+
+        //transform.Rotate(-Vector3.up, turnSpeed * Time.deltaTime);
+    }
+}
